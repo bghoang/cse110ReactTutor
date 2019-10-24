7826bbb67cb166b23996e6398ef2d511e9664061
Bao Hoang <38933533+bghoang@users.noreply.github.com>
2019-10-24 00:02:11 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import TeamMembers from "./components/TeamMembers";
+import DisplayData from "./components/DisplayData";
+
+function App() {
+  return (
+    <div className="App">
+      <TeamMembers />
+      <DisplayData />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/base.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+const db = firebase.initializeApp({
+  apiKey: "AIzaSyAzU6_dXTxYn2uc8t2kSGL5EomrNFX4CPY",
+  authDomain: "cse110firebase-96015.firebaseapp.com",
+  databaseURL: "https://cse110firebase-96015.firebaseio.com",
+  projectId: "cse110firebase-96015",
+  storageBucket: "cse110firebase-96015.appspot.com",
+  messagingSenderId: "45579713085",
+  appId: "1:45579713085:web:e22bf600f130d847a44854",
+  measurementId: "G-3FP0LM958S"
+});
+
+export default db;
